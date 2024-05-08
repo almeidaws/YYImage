63a809cdc0628d94260a847bee8f9c7b4f948966
Gustavo Amaral <almeidaws@users.noreply.github.com>
2024-05-08 09:33:35 -0300

    Add SPM compatibility

--- Package.swift
+++ Package.swift
@@ -20,9 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "YYImage",
-            dependencies: []),
-        .testTarget(
-            name: "YYImageTests",
-            dependencies: ["YYImage"]),
+            dependencies: [],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath(".")
+            ])
     ]
 )
--- Sources/YYImage/YYImage.swift
+++
@@ -1,6 +0,0 @@
-public struct YYImage {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
